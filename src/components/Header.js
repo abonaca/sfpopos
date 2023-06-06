dc8ad085ee57e14a171e6b8e644c829d8d0d7bc5
Ana Bonaca <abonaca@gmail.com>
2023-06-05 18:56:32 -0700

    header component built

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+// src/components/Header.js
+
+import React from 'react'
+import '../stylesheets/Header.css';
+
+function Header() {
+  return (
+    <div className="Title">
+      <h1>SFPOPOS</h1>
+      <h2 className="Title-Subtitle">San Francisco's Privately Owned Public Spaces</h2>
+    </div>
+  )
+}
+
+export default Header
